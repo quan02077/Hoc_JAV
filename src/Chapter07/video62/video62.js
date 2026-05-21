2f3310af13876f04ec8fcd2c0757a2c7d826e3e5
Quan Nguyen <quan02077@gmail.com>
2026-05-21 22:36:20 +0700

    Promise

---
+++ src/Chapter07/video62/video62.js
@@ -0,0 +1,26 @@
+console.log('Video 62');
+
+// console.log("1");
+// //async
+// setTimeout(() => {
+//     console.log("2");
+// }, 5000)
+
+// console.log("3");
+
+const myPromise = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("2 with promise");
+            resolve("Promise Thành công");
+        }, 2000)
+    })
+}
+const test = myPromise();
+console.log(test)
+
+console.log("1")
+myPromise().then(data => {
+    console.log(data);
+    console.log("3")
+})
